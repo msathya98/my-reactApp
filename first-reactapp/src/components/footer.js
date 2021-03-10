bc3dd59e9191c20ff7cad72a7d1f89326e3ef119
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-10 19:43:05 +0530

    footer.js component for react-app with tailwind css.

---
+++ first-reactapp/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class footer extends React.Component {
+    render() {
+        return (
+           <footer className="bg-gray-200 text-center text-xs p-4 absolute bottom-0 w-full">
+               @copyrights by Zeroswap labs 2021
+           </footer>
+        )
+}
+
+
+}
+export default footer; 
