2eef87104697755ecabd2e6c2e0bb8d53ec20367
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-10 19:51:30 +0530

    header.js component of react-app updated.

---
+++ first-reactapp/src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function header() {
+
+    return (
+        <div>
+            <header>
+            <h1 className='border-b font-bold p-7 text-center '>Hello Guys</h1>
+            </header>
+           
+
+        </div>
+
+    
+    )
+}
+
+export default header;
