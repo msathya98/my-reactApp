04b7a267ebf6440a5245e571a09e4696967fa74e
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-11 20:03:46 +0530

    app.js with router, switch, route and Link components updated.

--- first-reactapp/src/app.js
+++ first-reactapp/src/app.js
@@ -1,20 +1,48 @@
 import React from 'react';
-import Helloworld from "./components/helloworld";
-import Message from "./components/message";
-import Footer from "./components/footer";
-import Navigation from "./components/navigation";
+import Header from "./components/Header";
+import Message from "./components/Message";
+import Footer from "./components/Footer";
+import Home from "./Views/Home";
+import About from "./Views/About";
+
+
 //import Helloworld from "./components/helloworld";
 import "tailwindcss/tailwind.css"
+
+import { BrowserRouter as Router,
+Switch,
+Route,
+Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-<Helloworld />
+    <Header />
     <Message />
-    <Navigation />
     <Footer />
+    <Router >
+      <ul>
+        <li>
+          <Link to="/" className="text-blue-500">Home</Link>
+        </li>
+        <li>
+          <Link to="/about" className="text-blue-500">About</Link>
+        </li>
+      </ul>
    
-    
+   
+   <Switch>
+     <Route exact path="/">
+       <Home />
+     </Route>
+
+     <Route path="/about">
+     <About />
+     </Route>
+
+   </Switch>
+   </Router>
 
     </div>
   );
