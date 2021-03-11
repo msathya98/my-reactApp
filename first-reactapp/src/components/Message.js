935e04b1dd589c81843a13ad2e0761d587e8fb3d
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-11 20:04:26 +0530

    Message.js component of react-app updated.

---
+++ first-reactapp/src/components/Message.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Message extends React.Component {
+    render() {
+        return (
+            <div className="p-4 ">
+<h2 className="font-bold">This is the home page.</h2>
+<p >By Sathya M</p>
+            </div>
+            
+        )
+}
+
+
+}
+export default Message; 
