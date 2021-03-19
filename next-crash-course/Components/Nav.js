92a8cb75c9b74880f6e872221c1bea20bbfb5986
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-03-19 19:58:05 +0530

    Navigation file for Home, About navbars updated.

---
+++ next-crash-course/Components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/Link';
+import navStyles from "../styles/Home.module.css";
+
+const Nav = () => {
+    return (
+        <nav className={navStyles.nav}>
+            <ul>
+                <li>
+                    <Link href="./">Home</Link>
+                </li>
+                <li>
+                    <Link href="./about">About</Link>
+                </li>
+            </ul>
+            
+        </nav>
+    )
+}
+
+export default Nav
